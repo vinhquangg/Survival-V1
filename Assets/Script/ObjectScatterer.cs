3396f6b5bd24d486ca36325812348bf48e74323a
QuangVinh <lequangvinh2001@gmail.com>
2025-06-13 21:14:15 +0700

    New

--- Assets/Script/ObjectScatterer.cs
+++ Assets/Script/ObjectScatterer.cs
@@ -1,16 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ObjectScatterer : MonoBehaviour
 {
-    public GameObject objectToSpawn;
-    public int spawnCount;
-    public float scatterRadius;
+    [Header("Danh sách prefab có trọng số spawn")]
+    public List<SpawnablePrefab> spawnablePrefabs;
 
-    public float spawnHeight;
-
+    public int spawnCount = 50;
+    public float scatterRadius = 20f;
+    public float spawnHeight = 10f;
     public LayerMask groundLayer;
 
-    void Start()
+    private void Start()
     {
         ScatterObjects();
     }
@@ -21,16 +22,36 @@ public class ObjectScatterer : MonoBehaviour
         {
             Vector3 randomPos = GetRandomPosition();
 
-            
-
             if (Physics.Raycast(randomPos, Vector3.down, out RaycastHit hit, 100f, groundLayer))
             {
-                float heightOffset = objectToSpawn.GetComponent<Renderer>().bounds.extents.y;
-                Vector3 spawnPos = hit.point + Vector3.up * heightOffset;
-                Instantiate(objectToSpawn, spawnPos, Quaternion.Euler(0, Random.Range(0, 360), 0));
+                GameObject selectedPrefab = GetRandomPrefabByWeight();
+                if (selectedPrefab == null) continue;
 
+                float offset = selectedPrefab.GetComponent<Renderer>()?.bounds.extents.y ?? 0.5f;
+                Vector3 spawnPos = hit.point + Vector3.up * offset;
+
+                Instantiate(selectedPrefab, spawnPos, Quaternion.Euler(0, Random.Range(0, 360), 0));
             }
         }
+    }
+
+    GameObject GetRandomPrefabByWeight()
+    {
+        int totalWeight = 0;
+        foreach (var prefab in spawnablePrefabs)
+            totalWeight += prefab.weight;
+
+        int randomValue = Random.Range(0, totalWeight);
+        int currentSum = 0;
+
+        foreach (var prefab in spawnablePrefabs)
+        {
+            currentSum += prefab.weight;
+            if (randomValue < currentSum)
+                return prefab.prefab;
+        }
+
+        return null;
     }
 
     Vector3 GetRandomPosition()
@@ -39,4 +60,13 @@ public class ObjectScatterer : MonoBehaviour
         Vector3 basePosition = transform.position;
         return new Vector3(basePosition.x + randomCircle.x, basePosition.y + spawnHeight, basePosition.z + randomCircle.y);
     }
+}
+
+
+[System.Serializable]
+public class SpawnablePrefab
+{
+    public GameObject prefab;
+    [Range(1, 100)]
+    public int weight = 1;
 }

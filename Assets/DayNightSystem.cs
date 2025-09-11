660d67b93df953843b7554618d15468f72c027e6
vinhquangg <85091906+vinhquangg@users.noreply.github.com>
2025-09-11 10:06:32 +0700

    Make daynight system

--- Assets/DayNightSystem.cs
+++ Assets/DayNightSystem.cs
@@ -1,18 +1,57 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Light directionLight;
+    public float dayDurationInSeconds = 24.0f; // Thời gian một ngày (giây)
+    public int currentHour;
+
+    float currentTimeOfDay = 0.0f;
+    public List<SkyBoxTimeMapping> timeMappings;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTimeOfDay += (Time.deltaTime / dayDurationInSeconds); // Chuyển đổi thời gian thành giờ
+        currentTimeOfDay %= 1;
+        currentHour = Mathf.FloorToInt(currentTimeOfDay * 24f);
+
+        directionLight.transform.rotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90f, 170f, 0f);
+
+        ChangeSkyBoxForCurrentHour();
     }
+
+    public void ChangeSkyBoxForCurrentHour()
+    {
+        Material currentSkyBox = null;
+        foreach (SkyBoxTimeMapping mapping in timeMappings)
+        {
+            if (currentHour == mapping.hour)
+            {
+                currentSkyBox = mapping.skyboxMaterial;
+                break;
+            }
+
+        }
+
+        if (currentSkyBox != null)
+        {
+            RenderSettings.skybox = currentSkyBox;
+        }
+    }
+}
+
+[Serializable]
+public class SkyBoxTimeMapping
+{
+    public string phaseName;
+    public int hour;
+    public Material skyboxMaterial;
 }
